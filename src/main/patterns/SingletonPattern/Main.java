d55e58685b7a0c5ac41fb121af767d753d4bbf07
liza <liza.shpinkovaa@mail.ru>
2020-08-17 12:33:41 +0300

    add Singleton pattern

--- src/main/patterns/SingletonPattern/Main.java
+++ src/main/patterns/SingletonPattern/Main.java
@@ -2,8 +2,8 @@ package main.patterns.SingletonPattern;
 
 public class Main {
     public static void main(String[] args) {
-        Singleton singleton = Singleton.getObject("abc");
-        Singleton singleton1 = Singleton.getObject("abcd");
+        Singleton singleton = Singleton.getObject("obj");
+        Singleton singleton1 = Singleton.getObject("obj2");
         System.out.println(singleton.value);
         System.out.println(singleton1.value);
     }
