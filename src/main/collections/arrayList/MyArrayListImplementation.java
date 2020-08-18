235f8a339e58d1454b127ebe7f6eb4e99ba6251f
liza <liza.shpinkovaa@mail.ru>
2020-08-18 16:16:52 +0300

    Add Queue, LinkedList, ArrayList and Map implementations

---
+++ src/main/collections/arrayList/MyArrayListImplementation.java
@@ -0,0 +1,32 @@
+package main.collections.arrayList;
+
+import java.util.Arrays;
+
+public class MyArrayListImplementation<T> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 5;
+    private Object elements[];
+
+    public MyArrayListImplementation() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(T element) {
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        elements[size++] = element;
+    }
+
+    private void ensureCapacity() {
+        elements = Arrays.copyOf(elements, elements.length * 2);
+    }
+
+    public T get(int i) {
+        if (i >= size || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + "Size: " + size);
+        }
+        return (T) elements[i];
+    }
+
+}
